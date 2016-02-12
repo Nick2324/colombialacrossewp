5e81a284ee70068d4fba94067dd0ceebbd07f70d
Nick2324 <nicolas.2324@gmail.com>
2016-02-11 21:54:44 -0500

    First templating

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,22 @@
+<?
+<!DOCTYPE html> 
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="cabecera">
+
+	</div>
+	<div class="menu_principal">
+	</div>
+	<div class="menu_bloque_conocimiento">
+	</div>
+	<div class="feed_redes_sociales">
+	</div>
+	<div class="contenido">
+
+	</div>
+</body>
+</html>
+?>
