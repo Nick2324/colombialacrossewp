d6a15bd98b06dc4cfaf15d3e30ea80ddcce9e7ff
Nicolas Garcia <nicolas.2324@gmail.com>
2016-02-12 12:06:36 -0500

    Se agrega controlador Home. Se agregan dependencias

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,12 +1,19 @@
-<?
+
 <!DOCTYPE html> 
 <html>
 <head>
-	<title></title>
+	<title>Colombia Lacrosse</title>
+	<link rel="stylesheet" href="./lib/thirdparty/bootstrap-3.3.6-dist/css/bootstrap.min.css"/>
+	<script src="./lib/thirdparty/underscore.js" type="text/javascript"></script>
+	<script src="./lib/thirdparty/jquery-2.2.0.min.js" type="text/javascript"></script>
+	<script src="./lib/thirdparty/handlebars-v4.0.5.js" type="text/javascript"></script>
+	<script src="./lib/thirdparty/backbone.js" type="text/javascript"></script>
+	<script src="./lib/thirdparty/bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
+{{$mivariable = 2014;}}
+ <p>HOLA COLOMBIA LACROSSE!! {{$mivariable}}</p>
 	<div class="cabecera">
-
 	</div>
 	<div class="menu_principal">
 	</div>
@@ -15,8 +22,6 @@
 	<div class="feed_redes_sociales">
 	</div>
 	<div class="contenido">
-
 	</div>
 </body>
 </html>
-?>
